46df128f8ac7e72e49c5d57a6b98eb09bc7c8e38
DnyaneshaDeepakPhadtare <78483256+DnyaneshaDeepakPhadtare@users.noreply.github.com>
2021-04-23 18:48:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("my first console project");
+var first_name="mina";
+var last_name="patil";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var number_1=6;
+var number_2=2; 
+var adittion=number_1+number_2;
+console.log(adittion);
+var subtraction=number_1-number_2;
+console.log(subtraction);
+var multiplication=number_1*number_2;
+console.log(multiplication);
+var divison=number_1/number_2;
+console.log(divison);
